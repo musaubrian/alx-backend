b78fcae6c63cb5da926f182a59868afbd438b8ae
Ernest <musaubrian15@gmail.com>
2023-01-24 15:21:21 +0300

    chore: create class BasicCache that inherits from class BaseCaching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    class is caching system inheriting the BaseCache
+
+    Args::
+        base_caching: class BaseCaching
+    """
+
+    def put(self, key, item):
+        """
+        assigns the item value for the key [key]
+        """
+        if key is None or item is None:
+            pass
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        return the value in self.cache_data linked to key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
