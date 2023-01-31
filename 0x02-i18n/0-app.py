35f0d1f3d2be13728f2a77f90c25bbe8fe13f3b3
Ernest <musaubrian15@gmail.com>
2023-01-31 15:13:19 +0300

    chore: instantiate a Babel object in the flask app.

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello_world() -> str:
+def hello_world():
     """
     returns a html template that prints
     `Welcome to Holberton` as a title
