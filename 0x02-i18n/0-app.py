6ff1bb9ba147fe48da0d376e2485bd5044313c06
Ernest <musaubrian15@gmail.com>
2023-01-31 14:50:46 +0300

    chore: create a simple flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Sets up a basic flask app"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world() -> str:
+    """
+    returns a html template that prints
+    `Welcome to Holberton` as a title
+    and `Hello world`as a header
+    """
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
