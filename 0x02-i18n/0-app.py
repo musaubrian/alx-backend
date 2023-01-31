89fb86f9730d946f318d316525fe2b82d4447b91
Ernest <musaubrian15@gmail.com>
2023-01-31 14:53:30 +0300

    fix: rename template to templates

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def hello_world() -> str:
     """
     returns a html template that prints
