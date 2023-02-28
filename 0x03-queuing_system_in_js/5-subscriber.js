f504ca0afafb785ab30f20007a7c9502c3465a2d
Ernest <musaubrian15@gmail.com>
2023-02-28 16:58:35 +0300

    chore: subscribing and publishing to channels

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,15 @@
+const redis = require('redis');
+
+const client = redis.createClient();
+client.on('connect', () => console.log('Redis client connected to the server'));
+client.on('error', err => console.log('Redis client not connected to the server:', err));
+client.subscribe('holberton school channel', () => {
+  client.on('message', (channel, message) => {
+    if (message === 'KILL_SERVER') {
+      client.unsubscribe('holberton school channel');
+      client.quit();
+    } else {
+      console.log(message);
+    }
+  });
+});
