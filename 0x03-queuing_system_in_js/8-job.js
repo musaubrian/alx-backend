d67dd89ebd55ddaa69bb568c255b1ce710be8467
Ernest <musaubrian15@gmail.com>
2023-03-02 09:24:35 +0300

    chore: job creation function
    Takes in two arguments, jobs and a queue(Kue)

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,21 @@
+module.exports = function createPushNotificationsJobs (jobs, queue) {
+  if (!jobs instanceof Array) throw new Error('Jobs is not an array');
+
+  jobs.forEach(jobData => {
+    const job = queue.create('push_notification_code_3', jobData)
+      .save((error) => {
+        if (!error) console.log(`Notification job created: ${job.id}`);
+      });
+    job.on('complete', () => {
+      console.log(`Notification job ${job.id} completed`);
+    });
+
+    job.on('failed', (errorMessage) => {
+      console.log(`Notification job ${job.id} failed: ${errorMessage}`);
+    });
+
+    job.on('progress', (progress) => {
+      console.log(`Notification job ${job.id} ${progress}% complete`);
+    });
+  });
+};
